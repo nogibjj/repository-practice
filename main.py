a6dcd86eca6ba67688d593e8e6b35f42d07e8e1f
qu-genesis <60377132+qu-genesis@users.noreply.github.com>
2022-09-08 00:49:05 +0000

    created notebook

--- main.py
+++ main.py
@@ -2,4 +2,8 @@
 import dask
 import dask.dataframe as dd
 
-# read in kaggle data
+# read in watson_healthcare data
+ddf = dd.read_csv('watson_healthcare_modified.csv')
+
+# print the first 5 rows
+print(ddf.head())
